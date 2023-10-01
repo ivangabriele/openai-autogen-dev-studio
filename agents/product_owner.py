ad0342c074b8c7c92d6f08b8a494e4e7ddc1ba66
Ivan Gabriele <ivan.gabriele@gmail.com>
2023-10-02 00:39:08 +0200

    ci(github): add type job

--- agents/product_owner.py
+++ agents/product_owner.py
@@ -1,5 +1,3 @@
-import select
-from typing import TYPE_CHECKING
 from autogen import AssistantAgent
 from agents.base_agent import BaseAgent
 from constants import COMMON_LLM_CONFIG
@@ -7,15 +5,7 @@ from constants import COMMON_LLM_CONFIG
 from utils import clean_text
 
 
-if TYPE_CHECKING:
-    from autogen import UserProxyAgent
-    from agents.software_engineer import SoftwareEngineer
-
-
 class ProductOwner(BaseAgent):
-    ceo_user_proxy_agent: "UserProxyAgent"
-    software_engineer: "SoftwareEngineer"
-
     def __init__(self) -> None:
         self.is_product_owner = True
 
@@ -44,15 +34,4 @@ class ProductOwner(BaseAgent):
                 The CEO can help when your team is stuck.
                 """
             ),
-        )
-
-    def attach_agents(
-        self,
-        software_engineer: "SoftwareEngineer",
-    ):
-        self.software_engineer = software_engineer
-
-    def ask_software_engineer(self, message: str):
-        return self.software_engineer.ask(
-            sender=self.as_assistant_agent, message=message
         )
